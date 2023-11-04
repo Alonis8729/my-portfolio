2b7002d827726cea4b45d811cb7ad819139fffcd
Alonso8729 <alon8729@gmail.com>
2023-11-04 12:07:25 +0200

    Modified openTab function. Removed index parameter

---
+++ src/modules/dom.js
@@ -0,0 +1,19 @@
+const dom = (() => {
+  function openTab() {
+    const tabLinks = document.querySelectorAll('.tab-link')
+    const tabContents = document.querySelectorAll('.tab-contents')
+
+    tabLinks.forEach((link) => link.classList.toggle('active-link'))
+    tabContents.forEach((tab) => {
+      tab.classList.toggle('show')
+      tab.classList.toggle('hide')
+    })
+
+  }
+
+  return {
+    openTab,
+  }
+})()
+
+export default dom
