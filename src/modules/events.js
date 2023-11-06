6b7258ae194c994ac001d546f7c598d0033ba8d2
Alonso8729 <alon8729@gmail.com>
2023-11-06 16:07:42 +0200

    Fixed typo

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,39 +1,38 @@
+/* eslint-disable no-alert */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
-import dom from './dom'
+import dom from './dom';
 
 const events = (() => {
-  const menu = document.querySelector('.menu')
+  const menu = document.querySelector('.menu');
 
   function clickListener() {
     document.addEventListener('click', (event) => {
-      const { target } = event
-
+      const { target } = event;
+      console.log(target);
       // Click on About tab link
       if (target.classList.contains('tab-link')) {
         if (!target.classList.contains('active-link')) {
-          dom.openTab()
+          dom.openTab();
         }
       }
 
       if (target.classList.contains('menu-link')) {
-        if (!target.classList.contains('active-link')) {
-          dom.openMenuLink(target)
-        }
+        dom.openMenuLink(target);
       }
 
       // Navbar manipulation
       if (target.classList.contains('menu-btn')) {
-        menu.style.right = '0'
+        menu.style.right = '0';
       } else if (target.classList.contains('close-btn')) {
-        menu.style.left = '-200px'
+        menu.style.right = '-200px';
       }
-    })
+    });
   }
 
   return {
     clickListener,
-  }
-})()
+  };
+})();
 
-export default events
+export default events;
