d0329f4fecc1ab4bd458ac732c9ab87fdbffc2fc
Alonso8729 <alon8729@gmail.com>
2023-11-07 09:44:07 +0200

    FIxed typo

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,7 +2,7 @@ const dom = (() => {
   function openTab() {
     const tabLinks = document.querySelectorAll('.tab-link');
     const tabContents = document.querySelectorAll('.tab-contents');
-
+   
     tabLinks.forEach((link) => link.classList.toggle('active-link'));
     tabContents.forEach((tab) => {
       tab.classList.toggle('hide');
@@ -11,7 +11,7 @@ const dom = (() => {
 
   function openMenuLink(link) {
     const menuLinks = document.querySelectorAll('.menu-link');
-    menuLinks.forEach((menuLink) => menuLink.classList.remove('.active-link'));
+    menuLinks.forEach((menuLink) => menuLink.classList.remove('active-link'));
     link.classList.add('active-link');
   }
 
